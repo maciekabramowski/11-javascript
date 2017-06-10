1da5c6fe417058d9bc4fd926306d44a7e418dfe8
Maciej Abramowski <m.abramowski@wp.pl>
2017-06-10 16:21:11 +0200

    operacje 2

--- js/11-arrays-cont.js
+++ js/11-arrays-cont.js
@@ -45,8 +45,63 @@ console.log(imiona);
 
 console.log(imiona.length);
 
+// pętla przez tabelę
+
 for (var i=0; i < imiona.length; i++) {
     console.log(imiona[i]);
 }
 
+//pętla przez tabelę alternatywną metodą
+
+imiona.forEach(function(element, i){
+               console.log('Element nr ' + i + ' = ' + element);
+               });
+
+// łączenie elementów
+
+console.log(imiona.join(' - '));
+
+// sortowanie
+
+imiona.sort();
+
+console.log(imiona);
+
+//odwrócenie
+
+imiona.reverse();
+
+console.log(imiona);
+
+
+
+//łączenie tablic
+
+var imionaMeskie = ['Robert', 'Mariusz', 'Andrzej'];
+
+var zbiorImion = imionaMeskie.concat(imiona);
+
+console.log(zbiorImion);
+
+//indexOf
+
+console.log(zbiorImion.indexOf('Mariusz'));
+
+//sprawdza czy jest tablicą
+
+console.log( Array.isArray(zbiorImion));
+
+//slice
+
+console.log(zbiorImion.slice(2,5));
+
+//splice
+
+zbiorImion.splice(2,3, "Julek", "Krzyś");
+
+console.log(zbiorImion);
+
+//to string
+
+console.log(zbiorImion.toString());
 
