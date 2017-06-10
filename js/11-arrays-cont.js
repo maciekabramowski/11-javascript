14672c5bf39bf8f65c19aa7bd355046894e983e0
Maciej Abramowski <m.abramowski@wp.pl>
2017-06-10 15:54:51 +0200

    operacje na tablicach

--- js/11-arrays-cont.js
+++ js/11-arrays-cont.js
@@ -1,1 +1,52 @@
 'use strict';
+
+var imiona = ['Monika', 'Krystian', 'Łukasz'];
+
+console.log(imiona);
+
+//dodanie
+
+imiona[3] = 'Tania';
+
+console.log(imiona);
+
+//podmiana
+
+imiona[2] = 'Ela';
+
+console.log(imiona);
+
+//dodanie na nową pozycję
+
+imiona.push('Geralt');
+
+console.log(imiona);
+
+//usuwa ostatnią
+
+imiona.pop();
+imiona.pop();
+
+console.log(imiona);
+
+//wstawia na początku i zwraca ilość elementów
+
+console.log(imiona.unshift('Robert'));
+
+console.log(imiona);
+
+//usuwa pierwszy element
+
+imiona.shift();
+
+console.log(imiona);
+
+//wyświetla długość
+
+console.log(imiona.length);
+
+for (var i=0; i < imiona.length; i++) {
+    console.log(imiona[i]);
+}
+
+
